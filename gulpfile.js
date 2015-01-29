ea330a45869780f735daf1d83cad7388ea8becea
Sean Upton <sdupton@gmail.com>
2015-01-29 11:07:05 -0700

    style fixes to make gulpfile more readable and play nice with jshint/syntastic in my vim config

--- gulpfile.js
+++ gulpfile.js
@@ -1,149 +1,150 @@
 /*!
- * Facebook React Starter Kit | https://github.com/kriasoft/react-starter-kit
- * Copyright (c) KriaSoft, LLC. All rights reserved. See LICENSE.txt
+ * gulpfile.js for UPIQ data plot rendering https://github.com/upiq/plotqi
+ *  Modifications to boilerplate Copyright 2014-2015 University of Utah
+ *  Licensed under same MIT-style license as upstream.
+ * 
+ * UPSTREAM: Modified from boilerplate, via:
+ *  React Starter Kit | https://github.com/kriasoft/react-starter-kit
+ *  Copyright (c) KriaSoft, LLC. All rights reserved. See LICENSE.txt
  */
 
-'use strict';
+(function () {
+  'use strict';
 
-// Include Gulp and other build automation tools and utilities
-// See: https://github.com/gulpjs/gulp/blob/master/docs/API.md
-var gulp = require('gulp');
-var $ = require('gulp-load-plugins')();
-var del = require('del');
-var path = require('path');
-var merge = require('merge-stream');
-var runSequence = require('run-sequence');
-var webpack = require('webpack');
-var browserSync = require('browser-sync');
-var argv = require('minimist')(process.argv.slice(2));
-var defaultTask = (
+  // Include Gulp and other build automation tools and utilities
+  // See: https://github.com/gulpjs/gulp/blob/master/docs/API.md
+  var gulp = require('gulp');
+  var $ = require('gulp-load-plugins')();
+  var del = require('del');
+  var runSequence = require('run-sequence');
+  var webpack = require('webpack');
+  var browserSync = require('browser-sync');
+  var argv = require('minimist')(process.argv.slice(2));
+
+  // Settings
+  var DEST = './build';             // The build output folder
+  var RELEASE = !!argv.release;     // Minimize and optimize during a build?
+  var AUTOPREFIXER_BROWSERS = [     // https://github.com/ai/autoprefixer
+    'ie >= 9',
+    'ie_mob >= 9',
+    'ff ESR',
+    'chrome >= 34',
+    'safari >= 7',
+    'opera >= 23',
+    'ios >= 7',
+    'android >= 4.4',
+    'bb >= 10'
+  ];
+  var DEFAULT_TASK = (
     (Object.keys(argv).indexOf('serve') !== -1) ? 'serve' : 'build'
     );
 
-// Settings
-var DEST = './build';                         // The build output folder
-var RELEASE = !!argv.release;                 // Minimize and optimize during a build?
-var AUTOPREFIXER_BROWSERS = [                 // https://github.com/ai/autoprefixer
-  'ie >= 9',
-  'ie_mob >= 9',
-  'ff ESR',
-  'chrome >= 34',
-  'safari >= 7',
-  'opera >= 23',
-  'ios >= 7',
-  'android >= 4.4',
-  'bb >= 10'
-];
+  var src = {};
+  var watch = false;
+  var reload = browserSync.reload;
+  var pkgs = (function () {
+    var temp = {};
+    var map = function (source) {
+      for (var key in source) {
+        temp[key.replace(/[^a-z0-9]/gi, '')] = source[key].substring(1);
+      }
+    };
+    map(require('./package.json').dependencies);
+    return temp;
+  }());
 
-var src = {};
-var watch = false;
-var reload = browserSync.reload;
-var pkgs = (function () {
-  var temp = {};
-  var map = function (source) {
-    for (var key in source) {
-      temp[key.replace(/[^a-z0-9]/gi, '')] = source[key].substring(1);
-    }
-  };
-  map(require('./package.json').dependencies);
-  return temp;
-}());
+  // The default task
+  gulp.task('default', [DEFAULT_TASK]);
 
-// The default task
-gulp.task('default', [defaultTask]);
+  // Clean up
+  gulp.task('clean', del.bind(null, [DEST]));
 
-// Clean up
-gulp.task('clean', del.bind(null, [DEST]));
+  // Static files
+  gulp.task('assets', function () {
+    src.assets = 'src/assets/**';
+    return gulp.src(src.assets)
+      .pipe(gulp.dest(DEST))
+      .pipe($.if(watch, reload({stream: true})));
+  });
 
-// Static files
-gulp.task('assets', function () {
-  src.assets = 'src/assets/**';
-  return gulp.src(src.assets)
-    .pipe(gulp.dest(DEST))
-    .pipe($.if(watch, reload({stream: true})));
-});
+  // Images
+  gulp.task('images', function () {
+    src.images = 'src/images/**';
+    return gulp.src(src.images)
+      .pipe($.cache($.imagemin({
+        progressive: true,
+        interlaced: true
+      })))
+      .pipe(gulp.dest(DEST + '/images'))
+      .pipe($.if(watch, reload({stream: true})));
+  });
 
-// Images
-gulp.task('images', function () {
-  src.images = 'src/images/**';
-  return gulp.src(src.images)
-    .pipe($.cache($.imagemin({
-      progressive: true,
-      interlaced: true
-    })))
-    .pipe(gulp.dest(DEST + '/images'))
-    .pipe($.if(watch, reload({stream: true})));
-});
+  // HTML pages
+  gulp.task('pages', function () {
+    src.pages = 'src/pages/**/*.html';
+    return gulp.src(src.pages)
+      .pipe($.if(RELEASE, $.htmlmin({
+        removeComments: true,
+        collapseWhitespace: true,
+        minifyJS: true
+      })))
+      .pipe(gulp.dest(DEST))
+      .pipe($.if(watch, reload({stream: true})));
+  });
 
-// HTML pages
-gulp.task('pages', function () {
-  src.pages = 'src/pages/**/*.html';
-  return gulp.src(src.pages)
-    .pipe($.if(RELEASE, $.htmlmin({
-      removeComments: true,
-      collapseWhitespace: true,
-      minifyJS: true
-    })))
-    .pipe(gulp.dest(DEST))
-    .pipe($.if(watch, reload({stream: true})));
-});
-
-// CSS style sheets
-gulp.task('styles', function () {
-  src.styles = 'src/styles/**/*.{css,less}';
-  return gulp.src('src/styles/app.less')
+  // CSS style sheets
+  gulp.task('styles', function () {
+    src.styles = 'src/styles/**/*.{css,less}';
+    return gulp.src('src/styles/app.less')
     .pipe($.plumber())
-    .pipe($.less({sourceMap: !RELEASE, sourceMapBasepath: __dirname}))
-    .on('error', $.util.log)
-    .pipe($.autoprefixer(AUTOPREFIXER_BROWSERS))
-    .pipe($.csscomb())
-    .pipe($.if(RELEASE, $.minifyCss()))
-    .pipe(gulp.dest(DEST + '/css'))
-    .pipe($.if(watch, reload({stream: true})));
-});
+      .pipe($.less({sourceMap: !RELEASE, sourceMapBasepath: __dirname}))
+      .on('error', $.util.log)
+      .pipe($.autoprefixer(AUTOPREFIXER_BROWSERS))
+      .pipe($.csscomb())
+      .pipe($.if(RELEASE, $.minifyCss()))
+      .pipe(gulp.dest(DEST + '/css'))
+      .pipe($.if(watch, reload({stream: true})));
+  });
 
-// Bundle
-gulp.task('bundle', function (cb) {
-  var started = false;
-  var config = require('./config/webpack.js')(RELEASE);
-  var bundler = webpack(config);
+  // Bundle
+  gulp.task('bundle', function (cb) {
+    var started = false;
+    var config = require('./config/webpack.js')(RELEASE);
+    var bundler = webpack(config);
 
-  function bundle (err, stats) {
-    if (err) {
-      throw new $.util.PluginError('webpack', err);
+    function bundle (err, stats) {
+      if (err) {
+        throw new $.util.PluginError('webpack', err);
+      }
+      if (argv.verbose) {
+        $.util.log('[webpack]', stats.toString({colors: true}));
+      }
+      if (watch) {
+        reload(config.output.filename);
+      }
+      if (!started) {
+        started = true;
+        return cb();
+      }
     }
-
-    !!argv.verbose && $.util.log('[webpack]', stats.toString({colors: true}));
 
     if (watch) {
-      reload(config.output.filename);
+      bundler.watch(200, bundle);
+    } else {
+      bundler.run(bundle);
     }
+  });
 
-    if (!started) {
-      started = true;
-      return cb();
-    }
-  }
+  // Build the app from source code
+  gulp.task('build', ['clean'], function (cb) {
+    runSequence(['assets', 'images', 'pages', 'styles', 'bundle'], cb);
+  });
 
-  if (watch) {
-    bundler.watch(200, bundle);
-  } else {
-    bundler.run(bundle);
-  }
-});
-
-// Build the app from source code
-gulp.task('build', ['clean'], function (cb) {
-  runSequence(['assets', 'images', 'pages', 'styles', 'bundle'], cb);
-});
-
-// Launch a lightweight HTTP Server
-gulp.task('serve', function (cb) {
-
-  watch = true;
-
-  runSequence('build', function () {
-    browserSync({
+  // Launch a lightweight HTTP Server
+  gulp.task('serve', function (cb) {
+    watch = true;
+    runSequence('build', function () {
+      browserSync({
       notify: false,
       // Run as an https by uncommenting 'https: true'
       // Note: this uses an unsigned certificate which on first access
@@ -152,13 +153,15 @@ gulp.task('serve', function (cb) {
       server: {
         baseDir: ['build']
       }
-    });
+      });
 
-    gulp.watch(src.assets, ['assets']);
-    gulp.watch(src.images, ['images']);
-    gulp.watch(src.pages, ['pages']);
-    gulp.watch(src.styles, ['styles']);
-    cb();
+      gulp.watch(src.assets, ['assets']);
+      gulp.watch(src.images, ['images']);
+      gulp.watch(src.pages, ['pages']);
+      gulp.watch(src.styles, ['styles']);
+      cb();
+    });
   });
-});
+
+}());
 
