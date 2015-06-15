d11ef8e6e2c38679f9a08b17e047b55baefd3401
Sean Upton <sdupton@gmail.com>
2015-06-15 17:01:12 -0600

    remove legacy loader

--- src/scripts/graph.es6.js
+++ src/scripts/graph.es6.js
@@ -16,30 +16,3 @@ function readySetGo(callback) {
 
 readySetGo(loadReports);
 
-
-// vvv LEGACY LOADER: vvv
-readySetGo(
-  forReportJSON(
-      'report.json',
-      function (charts) {
-        var lineChart, barChart,
-            data = charts[0];
-
-        charts = charts.map( graph => new Chart(graph) );
-
-        lineChart = charts[0];
-        barChart = new Chart(data);
-        
-        window.charts = charts;
-
-        lineChart.width = 100;
-        barChart.width = 100;
-        barChart.chart_type = 'bar';
-
-        var refactorLinePlotDiv = d3.select('#chart-div-refactor-1');
-        var refactorBarPlotDiv = d3.select('#chart-div-refactor-2');
-        nv.addGraph(chartLoader(refactorLinePlotDiv, lineChart));
-        nv.addGraph(chartLoader(refactorBarPlotDiv, barChart));
-      }
-  )
-);
