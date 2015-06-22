2ce24a40f7b5807ec46c4a5d28f3b7e82eeb4b69
Sean Upton <sdupton@gmail.com>
2015-06-22 12:01:59 -0600

    remove cruft from old headless implementation

--- src/headless.js
+++
@@ -1,9 +0,0 @@
-/*jshint esnext:true, eqnull:true */
-/*globals require, window */
-
-'use strict';  /*jshint -W097 */
-require('./scripts/Symbol');
-require('es5-shim');
-var d3 = require('d3');
-var moment = require('moment');
-window.renderSVG = require('./scripts/renderSVG').renderSVG;
